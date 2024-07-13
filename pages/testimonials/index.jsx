2b064fc0f3bbbe42d87b0074b2f44206ae312560
Chirag Nahata <chiragnahata05@gmail.com>
2024-07-13 11:14:45 +0530

    Update index.jsx

--- pages/testimonials/index.jsx
+++ pages/testimonials/index.jsx
@@ -14,7 +14,7 @@ const Testimonials = () => {
           exit="hidden"
           className="h2 mb-8 xl:mb-0"
         >
-          My<span className="text-accent">Recommendations.</span>
+          My <span className="text-accent">Recommendations.</span>
         </motion.h2>
 
         {/* slider */}
