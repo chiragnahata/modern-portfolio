7598c2508e84ef5d5b442e72ae1f3b36611b0889
Chirag Nahata <chiragnahata05@gmail.com>
2024-07-14 10:50:51 +0530

    Update index.jsx

--- pages/testimonials/index.jsx
+++ pages/testimonials/index.jsx
@@ -1,28 +1,27 @@
 import { motion } from "framer-motion";
-
 import TestimonialSlider from "../../components/TestimonialSlider";
 import { fadeIn } from "../../variants";
 
 const Testimonials = () => {
   return (
-    <div className="h-full bg-primary/30 py-32 text-center">
-      <div className="container mx-auto h-full flex flex-col justify-center">
+    <div className="min-h-screen bg-primary/30 py-16 md:py-24 xl:py-32 text-center overflow-y-auto">
+      <div className="container mx-auto h-full flex flex-col justify-start xl:justify-center px-4 xl:px-0">
         <motion.h2
           variants={fadeIn("up", 0.2)}
           initial="hidden"
           animate="show"
           exit="hidden"
-          className="h2 mb-8 xl:mb-0"
+          className="text-3xl md:text-4xl xl:h2 mb-8 xl:mb-0"
         >
           Recommendations <span className="text-accent">Received.</span>
         </motion.h2>
-
         {/* slider */}
         <motion.div
           variants={fadeIn("up", 0.4)}
           initial="hidden"
           animate="show"
           exit="hidden"
+          className="flex-grow overflow-y-auto scrollbar-thin scrollbar-thumb-accent scrollbar-track-primary"
         >
           <TestimonialSlider />
         </motion.div>
